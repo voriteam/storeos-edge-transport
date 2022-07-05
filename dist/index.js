32e87c970e8c567d7ad5b6e389f6c284b0237502
Kent Wu <kent@vori.com>
2022-07-05 13:01:07 -0700

    Add File watcher

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,40 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = require("fastify");
+const chokidar_1 = __importDefault(require("chokidar"));
 const server = (0, fastify_1.fastify)({});
 const port = 8080;
+const STORE_ID = "TEST_STORE_ID";
+// TODO point this at the right ITRetail export directory
+// IMPORT/EXPORT is relative to Vori, not ITRetail
+const IMPORT_DIR = __dirname + "/../itretail_import";
+const EXPORT_DIR = __dirname + "/../itretail_export";
 const start = async () => {
     try {
         await server.listen({
             port: 8080,
         });
         console.log("Server started on port 8080");
+        console.log(IMPORT_DIR);
+        chokidar_1.default
+            .watch(IMPORT_DIR, {
+            ignored: /(^|[\/\\])\../,
+            persistent: true,
+        })
+            .on("all", (event, path) => {
+            console.log(event, path);
+        });
+        chokidar_1.default
+            .watch(EXPORT_DIR, {
+            ignored: /(^|[\/\\])\../,
+            persistent: true,
+        })
+            .on("all", (event, path) => {
+            console.log(event, path);
+        });
     }
     catch (err) {
         console.error(err);
--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
 import { fastify } from "fastify";
+import chokidar from "chokidar";
 const server = fastify({});
 
 const port = 8080;
+
+const STORE_ID = "TEST_STORE_ID";
+
+// TODO point this at the right ITRetail directories
+// IMPORT/EXPORT is relative to Vori, not ITRetail
+const IMPORT_DIR = __dirname + "/../itretail_import";
+const EXPORT_DIR = __dirname + "/../itretail_export";
 
 const start = async () => {
   try {
@@ -9,6 +17,25 @@ const start = async () => {
       port: 8080,
     });
     console.log("Server started on port 8080");
+    console.log(IMPORT_DIR);
+
+    chokidar
+      .watch(IMPORT_DIR, {
+        ignored: /(^|[\/\\])\../, // ignore dotfiles
+        persistent: true,
+      })
+      .on("all", (event, path) => {
+        console.log(event, path);
+      });
+
+    chokidar
+      .watch(EXPORT_DIR, {
+        ignored: /(^|[\/\\])\../, // ignore dotfiles
+        persistent: true,
+      })
+      .on("all", (event, path) => {
+        console.log(event, path);
+      });
   } catch (err) {
     console.error(err);
     process.exit(1);
